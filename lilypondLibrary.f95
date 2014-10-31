cf5f5e6c564b389c5179024832329a62da489c22
gilbertohasnofb <gilbertohasnofb@gmail.com>
2014-10-31 02:43:17 +0100

    initial commit

--- lilypondLibrary.f95
+++ lilypondLibrary.f95
@@ -1,28 +1,28 @@
 ! ********************************************************************************************************************************************************** !
-!																			     !
+!                                                                                                                                                            !
 !                                                     Library of Fortran Subroutines for LilyPond v2.0                                                       !
-!									LINUX VERSION									     !
-!																			     !
+!                                                                        LINUX VERSION                                                                       !
+!                                                                                                                                                            !
 !                                                     by Gilberto Agostinho (gilbertohasnofb@gmail.com)                                                      !
-!																			     !
-!                                                          (current version created on 28/10/2014)                                                           !
-!																			     !
+!                                                                                                                                                            !
+!                                                          (current version created on 31/10/2014)                                                           !
+!                                                                                                                                                            !
 ! ********************************************************************************************************************************************************** !
-!																			     !
-! IMPORTANT NOTES:																	     !
-!																			     !
-! - Tested with LilyPond version 2.19.15 (Linux) and GNU Fortran (Ubuntu 4.8.2-19ubuntu1) 4.8.2. 					                     !
-!																			     !
+!                                                                                                                                                            !
+! IMPORTANT NOTES:                                                                                                                                           !
+!                                                                                                                                                            !
+! - Tested with LilyPond version 2.19.15 (Linux) and GNU Fortran (Ubuntu 4.8.2-19ubuntu1) 4.8.2.                                                             !
+!                                                                                                                                                            !
 ! - Labels 7 and 11 are reserved (label 11 is used for writing the output .ly file. DO NOT USE ANY OF THEM DIRECTLY IN YOUR CODE!                            !
-!																			     !
+!                                                                                                                                                            !
 ! ********************************************************************************************************************************************************** !
-!																			     !
-! Usage:              															                     !
-!																			     !
-! - copy all these files to the directory of your Fortran code.												     !
-! - immediately before the program block, add the line: include "lilypondLibrary.f95"                                        			             !
-! - immediately after the program block, add the line: use lilypondLibrary					                                             !
-!																			     !
+!                                                                                                                                                            !
+! Usage:                                                                                                                                                     !
+!                                                                                                                                                            !
+! - copy all these files to the directory of your Fortran code.                                                                                              !
+! - immediately before the program block, add the line: include "lilypondLibrary.f95"                                                                        !
+! - immediately after the program block, add the line: use lilypondLibrary                                                                                   !
+!                                                                                                                                                            !
 ! ********************************************************************************************************************************************************** !
 
 module lilypondLibrary
@@ -48,7 +48,7 @@ contains
 	include "./subroutines/END_GRACE.f95"
 	include "./subroutines/END_POLY.f95"
 	include "./subroutines/END_REPEAT.f95"
-	include "./subroutines/END_REPEATBRACKET.f95"	
+	include "./subroutines/END_REPEATBRACKET.f95"          
 	include "./subroutines/END_STAFF.f95"
 	include "./subroutines/END_TUPLET.f95"
 	include "./subroutines/GLISSANDO.f95"
@@ -59,19 +59,19 @@ contains
 	include "./subroutines/MARK.f95"
 	include "./subroutines/MIDI_PITCH_TO_LP.f95"
  	include "./subroutines/NOTE.f95"
-	include "./subroutines/NOTEHEAD_STYLE.f95"	
+	include "./subroutines/NOTEHEAD_STYLE.f95"          
 	include "./subroutines/NOTEHEAD_TRANSPARENT.f95"
 	include "./subroutines/PAPER.f95"
 	include "./subroutines/PITCHEDTRILL.f95"
 	include "./subroutines/POLY.f95"
 	include "./subroutines/REPEAT.f95"
 	include "./subroutines/REPEATBRACKET.f95"
-	include "./subroutines/SNIPPET.f95"	
-	include "./subroutines/SNIPPET_REPEATBRACKET.f95"	! private subroutine
-	include "./subroutines/SNIPPET_SLASHEDGRACES.f95"	! private subroutine
-	include "./subroutines/SNIPPET_NATURALIZEMUSIC.f95"	! private subroutine
+	include "./subroutines/SNIPPET.f95"          
+	include "./subroutines/SNIPPET_REPEATBRACKET.f95"          ! private subroutine
+	include "./subroutines/SNIPPET_SLASHEDGRACES.f95"          ! private subroutine
+	include "./subroutines/SNIPPET_NATURALIZEMUSIC.f95"          ! private subroutine
 	include "./subroutines/SCORE.f95"
-	include "./subroutines/STAFF.f95"	
+	include "./subroutines/STAFF.f95"          
 	include "./subroutines/STAFF_LINE_COUNT.f95"
 	include "./subroutines/STEM.f95"
 	include "./subroutines/STOPSTAFF.f95"

43f89febf9ab1b6cc4b207cc216666593638c66f
gilbertohasnofb <gilbertohasnofb@gmail.com>
2015-11-30 13:35:44 +0100

    fixing small problem with SNIPPET_GRACE_MIDI_DURATION

--- src/subroutines/SNIPPET_GRACE_MIDI_DURATION.f95
+++ src/subroutines/SNIPPET_GRACE_MIDI_DURATION.f95
@@ -2,7 +2,7 @@ subroutine SNIPPET_GRACE_MIDI_DURATION(value)
 
 character (LEN=*), intent(IN) :: value
 
-write(*,"(A)") "#(define ac:defaultGraceFactor ", TRIM(value), ")"
-write(11,"(A)") "#(define ac:defaultGraceFactor ", TRIM(value), ")"
+write(*,"(A,A,A)") "#(define ac:defaultGraceFactor ", TRIM(value), ")"
+write(11,"(A,A,A)") "#(define ac:defaultGraceFactor ", TRIM(value), ")"
 
 end subroutine SNIPPET_GRACE_MIDI_DURATION
